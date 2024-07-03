9a4ac67f69f7f1c36595a52a51ef30ee37af93e3
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-07-03 11:40:47 +0300

    auto commit

--- Resources/Sample Client/MapReduceFramework.cpp
+++ Resources/Sample Client/MapReduceFramework.cpp
@@ -124,8 +124,8 @@ std::queue<IntermediateVec> __shuffle(ThreadContext *tc)
   std::queue<IntermediateVec> inter_queue;
   printf("vec0 size : %d\n ", tc->interVec[0]->size());
   printf("vec1 size : %d\n ", tc->interVec[1]->size());
-  printf("vec2 size : %d\n ", tc->interVec[2]->size());
-  printf("vec3 size : %d\n ", tc->interVec[3]->size());
+  // printf("vec2 size : %d\n ", tc->interVec[2]->size());
+  // printf("vec3 size : %d\n ", tc->interVec[3]->size());
   while (calculateProgress(tc->progress_counter) != 1)
   {
     K2 *max_key = findMaxKeyInLastPairs(tc->interVec, tc->multiThreadLevel);
